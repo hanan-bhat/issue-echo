2b79ec0ce2fda61c92bb1296ec03becb2c3e7437
hanan-bhat <bhat.hanan7@gmail.com>
2024-05-15 11:02:55 +0530

    initial push

---
+++ embed.js
@@ -0,0 +1,36 @@
+;(function () {
+  // Extract parameters from the script URL
+  function getQueryParams(script) {
+    var src = script.getAttribute('src')
+    var queryString = src.split('?')[1]
+    var params = {}
+    queryString.split('&').forEach(function (param) {
+      var pair = param.split('=')
+      params[pair[0]] = pair[1]
+    })
+    return params
+  }
+
+  // Find the script tag that includes this script
+  var scripts = document.getElementsByTagName('script')
+  var currentScript = scripts[scripts.length - 1]
+  var params = getQueryParams(currentScript)
+
+  // Create an iframe element
+  var iframe = document.createElement('iframe')
+  iframe.src =
+    'https://customerhearingsystem.netlify.app?account=' +
+    params['account'] +
+    '&project=' +
+    params['project'] +
+    '&author=' +
+    params[author] +
+    '&tenant=' +
+    params[tenant]
+  iframe.width = '100%'
+  iframe.height = '400px'
+  iframe.style.border = 'none'
+
+  // Append the iframe to the parent section
+  currentScript.parentNode.appendChild(iframe)
+})()
